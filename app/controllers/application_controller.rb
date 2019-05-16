008c8a447d2a0262ccd5b39603bdc650c7b9ff7e
Raphael Spencer <raphael@headway.io>
2019-05-16 11:45:30 -0500

    Parser/Interpreters are now included; AJAX form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  def show_html_builder
+    respond_to do |format|
+      format.html { render '/html_builder' }
+      format.js { render '/html_builder' }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#show_html_builder'
+  post '/', to: 'application#show_html_builder'
 end
